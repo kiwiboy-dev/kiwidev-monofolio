8d9be4ea7986ed8e7cd5929ddb2b29a598c8b644
KeithCheongWaiKeong <keithcheongwaikeong@gmail.com>
2024-07-17 18:00:59 +0800

    Deployment test with shared library

--- apps/hamsterfolio/src/app/app.tsx
+++ apps/hamsterfolio/src/app/app.tsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
+import { KiwidevUi } from '@kiwidev-monofolio/kiwidev-ui';
 
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
+      <KiwidevUi />
       <NxWelcome title="hamsterfolio" />
     </div>
   );
--- lib/src/lib/kiwidev-ui.tsx
+++ lib/src/lib/kiwidev-ui.tsx
@@ -3,7 +3,7 @@ import styles from './kiwidev-ui.module.scss';
 export function KiwidevUi() {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to KiwidevUi!</h1>
+      <h1>Welcome to KiwidevUi! I hope this works with AWS Amplify...</h1>
     </div>
   );
 }
